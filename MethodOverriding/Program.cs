3e2aa52dee1c5eaf48985115181b42c0d161b05e
Brittanie Campos <brittanie.angelique@gmail.com>
2022-06-26 15:52:49 -0700

    Add project files.

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    static void Main(String[] args)
+    {
+
+        // method overriding = provides a new version of a method inherited from a parent class
+        //                     inherited method must be: abstract, virtual, or already overriden
+        //                     Used with ToString(), polymorphism
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.Speak();
+        cat.Speak();
+
+        Console.ReadKey();
+
+
+    }
+}
+
+class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("The animal goes *brr*");
+    }
+}
+
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("The dog goes *woof*");
+    }
+}
+
+class Cat : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("The cat goes *meow*");
+    }
+}
